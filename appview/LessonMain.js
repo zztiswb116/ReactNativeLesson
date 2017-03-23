15f31015e7941187c4c05fcb8343fe08b53cf670
skyLee <zztiswb223@sina.com>
2017-03-23 17:47:02 +0800

    修改tabbar

--- appview/LessonMain.js
+++ appview/LessonMain.js
@@ -23,35 +23,37 @@ export default class LessonMain extends Component {
   };
 
   renderTabBarItem(title,selectedTab,image,selectedImage,component){
-    <TabNavigator.Item
-      selected={this.state.selectedTab === selectedTab}
-      title={title}
-      renderIcon={() => <Image source={{uri:image}} />}
-      renderSelectedIcon={() => <Image source={{uri:selectedImage}} />}
-      badgeText="1"
-      onPress={() => this.setState({ selectedTab: selectedTab })}>
-      <Navigator
-        initialRoute={{
-          name:selectedTab,
-          Component:component
-        }}
-        renderScene={(route,navigator) =>{
-          let Component = route.component;
-          return <Component {...route.params} navigator={navigator} />
-        }}
-      />
-    </TabNavigator.Item>
+    return(
+      <TabNavigator.Item
+        selected={this.state.selectedTab === selectedTab}
+        title={title}
+        renderIcon={() => <Image source={{uri:image}} />}
+        renderSelectedIcon={() => <Image source={{uri:selectedImage}} />}
+        badgeText="1"
+        onPress={() => this.setState({ selectedTab: selectedTab })}>
+        <Navigator
+          initialRoute={{
+            name:selectedTab,
+            component:component
+          }}
+          renderScene={(route,navigator) =>{
+            let Component = route.component;
+            return <Component {...route.params} navigator={navigator} />
+          }}
+        />
+      </TabNavigator.Item>
+    )
   };
 
   render(){
     return(
       <TabNavigator>
         {/* 首页 */}
-        {this.renderTabBarItem("首页",'home','','',LessonHome)}
+        {this.renderTabBarItem("首页",'home','tabbar_home_30x30','tabbar_home_selected_30x30',LessonHome)}
         {/* 海淘 */}
-        {this.renderTabBarItem("海淘",'shopping','','',LessonText)}
+        {this.renderTabBarItem("海淘",'shopping','tabbar_abroad_30x30','tabbar_abroad_selected_30x30',LessonText)}
         {/* 小时风云榜 */}
-        {this.renderTabBarItem("小时风云榜",'hourlist','','',LessonTextInput)}
+        {this.renderTabBarItem("小时风云榜",'hourlist','tabbar_rank_30x30','tabbar_rank_selected_30x30',LessonTextInput)}
       </TabNavigator>
     )
   };
